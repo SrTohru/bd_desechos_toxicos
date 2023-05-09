9eff8ca3d940dff1b9994023328e8259aef14c62
SrTohru <jose.hinojosa235666@potros.itson.edu.mx>
2023-05-09 10:52:59 -0700

    Inicio de proyecto

---
+++ src/main/java/org/itson/desechostoxicos/DesechosToxicos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.desechostoxicos;
+
+/**
+ *
+ * @author PC
+ */
+public class DesechosToxicos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
